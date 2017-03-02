e7d9fb0e9e56c82ccad830fcd556f6f0b1b060b3
Leiming Yu <leimingyu830@gmail.com>
2017-03-02 18:33:25 -0500

    profiling script

---
+++ mem_mem/profile_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for (( N=12000; N<=300000; N+=200 ))
+do
+
+nvprof --print-gpu-trace --csv ./mem_mem_ptx_all 2 0 $N 0 2> ./profile_results/trace_$N.csv
+
+nvprof --metrics all --csv ./mem_mem_ptx_all 2 0 $N 0 2> ./profile_results/metrics_$N.csv
+
+done
