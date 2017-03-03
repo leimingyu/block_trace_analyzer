2bad4ddcc4ad5eb7d53863444db877a9c749f8b3
Leiming Yu <leimingyu830@gmail.com>
2017-03-02 22:48:06 -0500

    profile

--- mem_mem/profile_app.sh
+++ mem_mem/profile_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for (( N=12000; N<=300000; N+=200 ))
+for (( N=12000; N<=300000; N+=1000 ))
 do
 
 nvprof --print-gpu-trace --csv ./mem_mem_ptx_all 2 0 $N 0 2> ./profile_results/trace_$N.csv
